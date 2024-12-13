19fdfa17ed7f718dbb544438f3e31c80507cbf6e
Masouma-kanso <masoumakanso@gmail.com>
2024-12-13 11:14:23 -0800

    created contact with feilds that detailed contact

---
+++ contact.java
@@ -0,0 +1,46 @@
+public class contact extends phoneNumber{
+    private String group;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private phoneNumber phoneNumber;
+    private address address;
+
+    public contact() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setGroup(String group) {
+    }
+
+    public void setEmail(String email) {
+    }
+
+    public void setphoneNumber(phoneNumber phoneNumber) {
+    
+    }
+
+    public void setaddress(address address) {
+    }
+}
+
+
